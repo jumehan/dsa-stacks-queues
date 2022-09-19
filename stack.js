6816dc5d7cc42d01156be56ab45f639d752d9cbf
Emily Chen <sticky_sugar@hotmail.com>
2022-09-19 16:19:24 -0700

    comeplete stack class

--- stack.js
+++ stack.js
@@ -19,26 +19,33 @@ class Stack {
   /** push(val): add new value to the top of the stack. Returns undefined. */
 
   push(val) {
-
+    let newNode = new Node(val);
+    newNode.next = this.top;
+    this.top = newNode;
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.size === 0) throw new Error;
+    let temp = this.top.val;
+    this.top = this.top.next;
+    this.size--;
+    return temp;
   }
 
   /** peek(): return the value of the top node in the stack. */
 
   peek() {
-
+    return this.top.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 ? true : false;
   }
 }
 
