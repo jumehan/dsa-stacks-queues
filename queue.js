5ae1244f92bdc27abee5b9133c37028b03e6e3a6
Julie Han <juliemenghan@gmail.com>
2022-09-19 19:10:32 -0400

    queue done and passed test

--- queue.js
+++ queue.js
@@ -23,7 +23,7 @@ class Queue {
     let newNode = new Node(val);
 
     if (this.first === null) this.first = newNode;
-    if (this.last === null) this.last = newNode;
+    if (this.last !== null) this.last.next = newNode;
     this.last = newNode;
     this.size++;
   }
@@ -32,19 +32,33 @@ class Queue {
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    // if queue is empty, throw error
+    if (this.size === 0) throw new Error;
 
+    let temp = this.first;
+
+    // if queue only has one element, first and last = null;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    temp.next = null;
+    this.size--;
+    return temp.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 ? true : false;
   }
 }
 
